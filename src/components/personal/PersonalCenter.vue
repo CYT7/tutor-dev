747efed267234e46e7fe90082a6017dd50af2eda
CYT7 <1490930847@qq.com>
2021-01-25 11:00:54 +0800

    修改个人中心

--- src/components/personal/PersonalCenter.vue
+++ src/components/personal/PersonalCenter.vue
@@ -3,35 +3,14 @@
     <el-col :span="20" :offset="2">
       <Header></Header>
     </el-col>
-    <div style="margin-top: 10px" align="center">
-      <el-table
-        :data="teacher_jian_Data.filter(data => !search || data.goodAt.toLowerCase().includes(search.toLowerCase()))"
-        style="width: 82%"
-        height="500"
-      ><!--height可实现固定表头的表格-->
-        <el-table-column align="center" label="家教 ID"  prop="id"></el-table-column>
-        <el-table-column align="center" label="教授科目" prop="goodAt"></el-table-column>
-        <el-table-column align="center" label="教学经验(年)" prop="experience"></el-table-column>
-        <el-table-column align="center" label="成功次数" prop="totalSuccess"></el-table-column>
-        <el-table-column align="center" label="在读/毕业院校" prop="school"></el-table-column>
-        <el-table-column align="center" label="年龄" prop="age"></el-table-column>
-        <el-table-column align="center" label="授课费用(时)" prop="hourPrice"></el-table-column>
-        <el-table-column align="center" label="所在城市" prop="city"></el-table-column>
-        <!--点击查看，跳转到家教页面详情-->
-        <el-table-column align="center" label="操作" width="150">
-          <template slot-scope="scope">
-            <router-link :to="{path:`/TeacherDetail`,name:`TeacherDetail`,params:{id:scope.row.id},query:{id:scope.row.id}}">
-              <el-button type="text" size="small" icon="el-icon-thumb">查看</el-button>
-            </router-link>
-          </template>
-        </el-table-column>
-        <!--搜索框（根据教授科目筛选家教信息）-->
-        <el-table-column align="right" width="190">
-          <template slot="header" slot-scope="scope">
-            <el-input prefix-icon="el-icon-search" v-model="search" size="mini" placeholder="输入教授科目进行筛选"/>
-          </template>
-        </el-table-column>
-      </el-table>
+    <div style="margin-top: 10px;margin-left:25%;width:20%;" align="center">
+      <el-form ref="form" :model="resultsMap" label-width="auto">
+        <el-form-item label="用户ID"><el-input v-model="resultsMap.id" disabled></el-input></el-form-item>
+        <el-form-item label="用户昵称"><el-input v-model="resultsMap.nickName" disabled></el-input></el-form-item>
+        <el-form-item label="旧密码" :rules="{ required: true, message: '旧密码不能为空', trigger: 'blur' }">
+          <el-input placeholder="请输入正在使用的密码" v-model="oldPassword" show-password clearable></el-input>
+        </el-form-item>
+      </el-form>
     </div>
   </el-row>
 </template>
@@ -43,7 +22,7 @@
     name: "PersonalCenter",
     data() {
       return {
-        teacher_jian_Data: [],
+        resultsMap: {},
         search: '',
         tokens : []
       }
@@ -51,23 +30,25 @@
     components: {
       Header
     },
-    mounted() {
-      axios.post('http://127.0.0.1:7001/business/user/information',{},{
-        headers:{
-          authorization:`Bearer ${Token}`
-        }
-      })
-        .then(
-          (res) => {
-            this.teacher_jian_Data = res.data.data
-            //数据拿到，ok!
-            console.log(this.teacher_jian_Data)
-          },
-          (err) => {
-            console.log(err);
+    created() {
+      this.getList();
+    },
+    methods:{
+      getList(){
+        axios.post('http://127.0.0.1:7001/business/user/information',{},{
+          headers:{
+            authorization:`Bearer ${Token}`
           }
-        )
-    }
+        }).then((res) => {
+            this.resultsMap = res.data.data
+            console.log(this.resultsMap)
+            },
+            (err) => {
+              console.log(err);
+            }
+          )
+      }
+    },
   }
 </script>
 <style scoped>
