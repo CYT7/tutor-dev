5915220bc04e7fbdd94798a5b4a15b4a7e114446
CYT7 <1490930847@qq.com>
2021-01-25 16:11:32 +0800

    添加个人预约和个人需求页面

---
+++ src/components/personal/PersonalAppointment.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "PersonalAppointment"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/personal/PersonalNeeds.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "PersonalNeeds"
+    }
+</script>
+
+<style scoped>
+
+</style>
