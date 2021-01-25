f9d86759464cb589a82a1b90532294a9d49e99a1
CYT7 <1490930847@qq.com>
2021-01-25 13:50:31 +0800

    修改个人中心

--- src/components/personal/PersonalCenter.vue
+++ src/components/personal/PersonalCenter.vue
@@ -4,12 +4,16 @@
       <Header></Header>
     </el-col>
     <div style="margin-top: 10px;margin-left:25%;width:20%;" align="center">
+      <h2 style="margin-left:15%">个人信息</h2>
       <el-form ref="form" :model="resultsMap" label-width="auto">
         <el-form-item label="用户ID"><el-input v-model="resultsMap.id" disabled></el-input></el-form-item>
         <el-form-item label="用户昵称"><el-input v-model="resultsMap.nickName" disabled></el-input></el-form-item>
-        <el-form-item label="旧密码" :rules="{ required: true, message: '旧密码不能为空', trigger: 'blur' }">
-          <el-input placeholder="请输入正在使用的密码" v-model="oldPassword" show-password clearable></el-input>
-        </el-form-item>
+        <el-form-item label="手机号码"><el-input v-model="resultsMap.phone" disabled></el-input></el-form-item>
+        <el-form-item label="邮箱"><el-input v-model="resultsMap.email" disabled></el-input></el-form-item>
+        <el-form-item label="QQ"><el-input v-model="resultsMap.qq" disabled></el-input></el-form-item>
+        <el-form-item label="微信号"><el-input v-model="resultsMap.wechat" disabled></el-input></el-form-item>
+        <el-form-item label="地址"><el-input v-model="resultsMap.address" disabled></el-input></el-form-item>
+        <el-form-item label="头像"><el-image style="height: 50px;width:50px" :src="resultsMap.image_url" fit="cover" /></el-form-item>
       </el-form>
     </div>
   </el-row>
