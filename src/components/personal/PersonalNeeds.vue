e4283b05ca64808b6b0389e2ac0c2910064dc13b
CYT7 <1490930847@qq.com>
2021-01-29 17:18:20 +0800

    重做发布需求

--- src/components/personal/PersonalNeeds.vue
+++ src/components/personal/PersonalNeeds.vue
@@ -1,18 +1,13 @@
 <template>
   <el-row :gutter="20">
-    <el-col :span="20"><Header></Header></el-col>
-    <el-col :span="20">
-      <div class="grid-content bg-purple" style="margin-top: 15px;margin-right:-100px;float: right">
-        <el-button type="primary" @click="dialogVisible2=true">+新建需求</el-button>
-      </div>
-    </el-col>
-    <div style="margin-top: 20px" align="center">
-      <el-table :data="resultsMap.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 80%" height="500"><!--height可实现固定表头的表格-->
+    <el-col :span="20" :offset="2"><Header></Header></el-col>
+    <div style="margin-top: 10px" align="center">
+      <el-table :data="resultsMap.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 82%" height="500"><!--height可实现固定表头的表格-->
         <el-table-column type="index" width="50" align="center" />
         <el-table-column align="center" label="需求id" prop="id"> </el-table-column>
         <el-table-column align="center" label="学生称呼" prop="nickName"> </el-table-column>
         <el-table-column align="center" label="授课科目" prop="subject"></el-table-column>
-        <el-table-column align="center" label="所在城市区域" prop="address" :formatter="formatAddress"></el-table-column>
+        <el-table-column align="center" label="所在城市区域" prop="address" :formatter="formatAddress" ></el-table-column>
         <el-table-column align="center" label="需求总报价(元)" prop="totalPrice"></el-table-column>
         <el-table-column align="center" label="需求发布时间" prop="createTime" :formatter="formatDate"></el-table-column>
         <el-table-column label="需求状态" prop="state" align="center">
@@ -28,7 +23,7 @@
         <!--点击查看，跳转到预约页面详情-->
         <el-table-column align="center" label="操作" width="150">
           <template slot-scope="scope">
-            <router-link :to="{path:`/NeedDetail`,name:`NeedDetail`,params:{id:scope.row.id},query:{id:scope.row.id}}">
+            <router-link :to="{path:`/TeacherNeed`,name:`TeacherNeed`,params:{id:scope.row.id},query:{id:scope.row.id}}">
               <el-button type="text" size="small" icon="el-icon-thumb">查看详情</el-button>
             </router-link>
           </template>
@@ -44,202 +39,22 @@
         :total="resultsMap.length">
       </el-pagination>
     </div>
-    <el-dialog title="新建需求" :visible.sync="dialogVisible2" width="30%" :before-close="handleClose">
-      <el-form ref="ruleForm2" :model="ruleForm2" :rules="rules2" label-width="140px" class="demo-ruleForm">
-        <el-form-item label="学生称呼" prop="nickName"><el-input v-model="ruleForm2.nickName" /></el-form-item>
-        <el-form-item label="性别" prop="gender">
-          <el-radio-group v-model="ruleForm2.gender" name="gender" style="float: left">
-            <el-radio v-for="item in gender" :key = "item.id" :label="item.id">{{item.name}}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="科目" prop="subject">
-          <el-select v-model="ruleForm2.subject" placeholder="请选择" style="float: left">
-            <el-option
-              v-for="item in catelist"
-              :key="item.id"
-              :label="item.name"
-              :value="item.name"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="预约上几次家教" prop="frequency"><el-input v-model="ruleForm2.frequency" /></el-form-item>
-        <el-form-item label="上课时间" prop="teach_date">
-          <el-cascader
-            v-model="ruleForm2.teach_date"
-            :options="times"
-            :props="{ expandTrigger: 'hover', multiple: true }"
-            style="float: left;"
-            @change="handleChange"></el-cascader>
-        </el-form-item>
-        <el-form-item label="每次上几小时" prop="timeHour"><el-input v-model="ruleForm2.timeHour" /></el-form-item>
-        <el-form-item label="上课地址" prop="address">
-          <el-cascader
-            style="float: left"
-            placeholder="请选择城市"
-            :options="options"
-            v-model="ruleForm2.address"
-            :show-all-levels="false">
-          </el-cascader>
-        </el-form-item>
-        <el-form-item label="课时费用" prop="hourPrice"><el-input v-model="ruleForm2.hourPrice" /></el-form-item>
-        <el-form-item label="家教性别要求" prop="teacherGender">
-          <el-radio-group v-model="ruleForm2.teacherGender" name="teacherGender" style="float: left">
-            <el-radio v-for="item in gender" :key = "item.id" :label="item.id">{{item.name}}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="联系方式" prop="phone"><el-input v-model="ruleForm2.phone" /></el-form-item>
-        <el-form-item label="QQ" prop="qq"><el-input v-model="ruleForm2.qq" /></el-form-item>
-        <el-form-item label="微信号" prop="wechat"><el-input v-model="ruleForm2.wechat" /></el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm2')">立即创建</el-button>
-          <el-button @click="resetForm('ruleForm2')">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </el-dialog>
   </el-row>
 </template>
 <script>
   import Header from "./Header";
-  import { regionData,CodeToText } from 'element-china-area-data'
-  import axios from 'axios'
+  import axios from 'axios';
+  import { CodeToText } from 'element-china-area-data'
   const tokens = localStorage.getItem('token');
   export default {
-    name: "PersonalNeeds",
+    name: "TeacherNeeds",
     data() {
       return {
         currentPage:1, //初始页
         pagesize:10,//每页的数据
         resultsMap: [],
-        tokens : [],
-        dialogVisible2:false,
-        ruleForm2:{
-          nickName:'',
-          gender:'',
-          teacherGender:'',
-          frequency:'',
-          timeHour:'',
-          teach_date:'',
-          hourPrice:'',
-          phone:'',
-          qq:'',
-          wechat:'',
-          address:[],
-          subject:'',
-        },
-        rules2: {
-          nickName: [{ required: true, message: '请输入学生称呼', trigger: 'blur' }]
-        },
-        options: regionData,// 城市数据
-        gender : [{
-          id:0,
-          name:'未知'
-        },{
-          id:1,
-          name:'男'
-        },{
-          id:2,
-          name:'女'
-        }],
-        times: [{
-          label:'星期一',
-          value: '星期一',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期二',
-          label: '星期二',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期三',
-          label: '星期三',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期四',
-          label: '星期四',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期五',
-          label: '星期五',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期六',
-          label: '星期六',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }, {
-          value: '星期日',
-          label: '星期日',
-          children: [{
-            label:'上午',
-            value:'上午'
-          },{
-            value:'下午',
-            label:'下午'
-          },{
-            value:'晚上',
-            label:'晚上'
-          }
-          ]
-        }],
-        catelist:'',
+        search: '',
+        tokens : []
       }
     },
     components: {
@@ -247,7 +62,6 @@
     },
     created(){
       this.getList()
-      this.getCateList()
     },
     methods:{
       getList(){
@@ -255,8 +69,9 @@
           headers:{
             authorization:`Bearer ${tokens}`
           }}).then(
-          (res) => {
-            this.resultsMap = res.data.data
+          (resp) => {
+            this.resultsMap = resp.data.data
+            //数据拿到，ok!
             console.log(this.resultsMap)
           },
           (err) => {
@@ -293,52 +108,6 @@
         const s =
           date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
         return Y + M + D + h + m + s
-      },
-      handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done()
-          })
-          .catch(_ => {})
-      },
-      getCateList(){
-        axios.get('http://127.0.0.1:7001/business/category/List',{
-          headers:{
-            authorization:`Bearer ${tokens}`
-          }
-        }).then(
-          (res) => {
-            const data = res.data.data;
-            this.catelist = data
-            console.log(this.catelist)
-          },
-          (err) => {
-            console.log(err);
-          }
-        )
-      },
-      submitForm(formName){
-        this.$refs[formName].validate(valid =>{
-          if (valid) {
-            axios.post('http://127.0.0.1:7001/business/need/create',this.ruleForm2,{
-              headers:{
-                authorization:`Bearer ${tokens}`
-              }
-            }).then(res => {
-              if (res.data.code === 0) {
-                this.$refs[formName].resetFields()
-                this.dialogVisible2 = false
-              }
-              console.log(res.data)
-            })
-          } else {
-            console.log('error submit!!')
-            return false
-          }
-        })
-      },
-      resetForm (formName) {
-        this.$refs[formName].resetFields()
       },
       formatAddress(row) {
         if (row.address === null) {
